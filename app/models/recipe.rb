9f64c2491b5de46a2c4c5d83d18629e06f9a362b
acsauk <acsauk@gmail.com>
2016-09-12 10:56:37 +0100

    creates failing tests for associations between recipe, recipeIngredient, ingredient

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
 
   belongs_to :user
+  
+  has_many :recipeIngredients
+  has_many :ingredients, through: :recipeIngredients
 
 end
--- spec/Models/recipe_spec.rb
+++ spec/Models/recipe_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
-
 describe Recipe, type: :model do
 
   it{ is_expected.to belong_to :user }
+  it{ is_expected.to have_many(:recipeIngredients) }
+  it{ is_expected.to have_many(:ingredients).through(:recipeIngredients) }
 
 end
