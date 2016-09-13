3a7f866b50ba26918281c52a246f7e35372306bb
Jose <jcopoviking@outlook.com>
2016-09-13 11:46:00 +0100

    sets up tests for meals view

---
+++ spec/Features/meal_feature_spec.rb
@@ -0,0 +1,18 @@
+feature 'meals' do
+  context 'creating meals' do
+    before { sign_up }
+    before { add_recipe }
+
+    scenario 'a user can create a meal' do
+      click_link 'Add a meal'
+      select 'Monday'
+      select 'Evening'
+      select 'Omelette'
+      click_button 'Create Meal'
+      expect(page).to have_content 'Monday'
+      expect(page).to have_content 'Evening'
+      expect(page).to have_content 'Omelette'
+      expect(current_path).to eq '/'
+    end
+  end
+end
--- spec/Helpers/recipe_helper.rb
+++ spec/Helpers/recipe_helper.rb
@@ -1,10 +1,11 @@
-def add_recipe(recipe: 'Omelette')
+def add_recipe(title: 'Omelette', ingredient1:"egg", quantity1:"2",
+                ingredient2:"cheese",quantity2:"200g" )
   click_link 'Add recipe'
-  fill_in 'Title', with: recipe
+  fill_in 'Title', with: title
   fill_in 'Instructions', with: 'Example cooking instructions'
-  fill_in("recipe[ingredients_attributes][0][name]", with: 'Egg')
-  fill_in('recipe[ingredients_attributes][0][quantity]', with: "2")
-  fill_in("recipe[ingredients_attributes][1][name]", with: 'Cheese')
-  fill_in("recipe[ingredients_attributes][1][quantity]", with: "200 grams")
+  fill_in("recipe[ingredients_attributes][0][name]", with: ingredient1 )
+  fill_in('recipe[ingredients_attributes][0][quantity]', with: quantity1 )
+  fill_in("recipe[ingredients_attributes][1][name]", with: ingredient2 )
+  fill_in("recipe[ingredients_attributes][1][quantity]", with: quantity2 )
   click_button 'Create Recipe'
 end
