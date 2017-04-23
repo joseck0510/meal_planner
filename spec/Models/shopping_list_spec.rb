5cbd2f381c942a21f98ce9ec2700b3cc3e233221
Alex Saunders <acsauk@Alexs-MacBook-Pro.local>
2017-04-23 14:45:35 +0100

    spacing on user spec. Creates failing test for ShoppingList model

---
+++ spec/Models/shopping_list_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe ShoppingList, type: :model do
+
+  it { is_expected.to belong_to :user }
+
+  it { is_expected.to have_many(:shoppingingredients) }
+  it { is_expected.to have_many(:ingredients).through(:shoppingingredients) }
+
+end
