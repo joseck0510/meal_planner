4b008f1c2e3d1b3ff7d332721a070ab04333be4e
Alex Saunders <acsauk@gmail.com>
2017-05-03 22:05:12 +0100

    tidies up recipes_feature_spec

--- spec/Features/recipes_feature_spec.rb
+++ spec/Features/recipes_feature_spec.rb
@@ -1,35 +1,31 @@
 feature 'Recipes' do
+  before(:each) do
+    sign_up
+    add_recipe
+  end
 
   context 'user signed in' do
     scenario 'adding recipes' do
-      sign_up
-      add_recipe
       expect(page).to have_content 'Omelette recipe successfully created'
       expect(current_path).to eq '/recipes'
     end
 
     scenario 'editing a recipe' do
-      sign_up
-      add_recipe
       visit '/recipes'
       click_link 'Edit recipe'
-      fill_in("Title", with: "Ham Omelette")
-      fill_in("recipe[ingredients_attributes][1][name]", with: 'Ham')
+      fill_in('Title', with: 'Ham Omelette')
+      fill_in('recipe[ingredients_attributes][1][name]', with: 'Ham')
       click_button 'Update Recipe'
       expect(page).to have_content 'Ham Omelette'
     end
 
     scenario 'deleting a recipe' do
-      sign_up
-      add_recipe
       visit '/recipes'
       click_link 'Delete recipe'
       expect(page).not_to have_content('Omelette')
     end
 
     scenario 'viewing a recipe' do
-      sign_up
-      add_recipe
       click_link 'My Recipes'
       click_link 'Omelette'
       expect(page).to have_content 'Omelette'
@@ -37,6 +33,4 @@ feature 'Recipes' do
       expect(page).to have_content '2'
     end
   end
-
-
 end
