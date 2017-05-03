ce0b9537e5768a90f6333caa710d38262b7837d7
Alex Saunders <acsauk@gmail.com>
2017-05-03 22:14:25 +0100

    tidies up user_feature_spec

--- spec/Features/users_feature_spec.rb
+++ spec/Features/users_feature_spec.rb
@@ -1,13 +1,15 @@
 feature 'User can sign in and out' do
   context 'user not signed in and on index' do
-    it 'should see "sign in" link and "sign up" link' do
+    before(:each) do
       visit '/'
+    end
+
+    it 'should see "sign in" link and "sign up" link' do
       expect(page).to have_link 'Sign in'
       expect(page).to have_link 'Sign up'
     end
 
     it 'should not see "Sign out" link' do
-      visit '/'
       expect(page).not_to have_link 'Sign out'
     end
   end
@@ -15,15 +17,14 @@ feature 'User can sign in and out' do
   context 'user signed in on the index' do
     before do
       sign_up
+      visit '/'
     end
 
     it 'should see "sign out" link' do
-      visit '/'
       expect(page).to have_link 'Sign out'
     end
 
     it 'should not see "sign up" and "sign in" links' do
-      visit '/'
       expect(page).not_to have_link 'Sign up'
       expect(page).not_to have_link 'Sign in'
     end
