28de078ae5cacacb68d820be4c91a6483db0e5a1
Alex Saunders <acsauk@gmail.com>
2017-05-03 21:51:44 +0100

    controller styling updates

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -1,13 +1,9 @@
 class PlannersController < ApplicationController
-
   def index
-
   end
 
   def search
-    if params[:search]
-      @recipes = Recipe.search(params[:search]).order("created_at DESC").uniq
-    end
+    return unless params[:search]
+    @recipes = Recipe.search(params[:search]).order('created_at DESC').uniq
   end
-
 end
