10dec7538c12d851aa69ee38be971d6370be7eaa
Alex Saunders <acsauk@gmail.com>
2017-05-04 21:59:09 +0100

    updates spec typo and includes association between tag and recipe

--- spec/Models/tag_spec.rb
+++ spec/Models/tag_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 describe Tag, type: :model do
-  it { is_expected.to have_many :recipes, through: :tag_recipes }
+  it { is_expected.to have_many(:recipes).through(:tag_recipes) }
 end
