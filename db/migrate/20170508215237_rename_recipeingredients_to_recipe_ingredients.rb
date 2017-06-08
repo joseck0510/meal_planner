bc6bffdf7b226c96caca293105e78987a1413ca7
Alex Saunders <acsauk@gmail.com>
2017-06-08 21:14:32 +0100

    drops migration to rename recipeingredients

--- db/migrate/20170508215237_rename_recipeingredients_to_recipe_ingredients.rb
+++
@@ -1,9 +0,0 @@
-class RenameRecipeingredientsToRecipeIngredients < ActiveRecord::Migration
-  def self.up
-    rename_table :recipeingredients, :recipe_ingredients
-  end
-
-  def self.down
-    rename_table :recipe_ingredients, :recipeingredients
-  end
-end
